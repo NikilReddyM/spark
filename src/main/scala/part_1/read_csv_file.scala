14c9177bf0568a76dba2774bbbaa6eed2db29260
NikilReddyM <nikilreddymamilla@gmail.com>
2020-04-20 14:57:59 -0500

    first commit

--- src/main/scala/part_1/read_csv_file.scala
+++ src/main/scala/part_1/read_csv_file.scala
@@ -3,15 +3,19 @@ package part_1
 import org.apache.spark.sql.SparkSession
 
 
-object read_csv_file extends App
+object read_csv_file
 {
-  val spark = SparkSession.builder()
-    .appName("projec-1")
-    .config("spark.master","local")
-    .getOrCreate()
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("projec-1")
+      //.config("spark.master","local")
+      .getOrCreate()
 
-  val dataDF = spark.read.json("src/main/resources/data/movies.json")
+    //val dataDF = spark.read.json("src/main/resources/data/movies.json")
+    //dataDF.show(10)
 
-  dataDF.show(10)
+    spark.conf.getAll.foreach(println)
+
+  }
 
 }
