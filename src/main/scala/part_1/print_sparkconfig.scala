75692e7b62613dd84e205f7b76450ff8a6c63c1f
NikilReddyM <nikilreddymamilla@gmail.com>
2020-04-23 14:04:16 -0500

    prints spark configuration parameters

--- src/main/scala/part_1/print_sparkconfig.scala
+++ src/main/scala/part_1/print_sparkconfig.scala
@@ -3,12 +3,12 @@ package part_1
 import org.apache.spark.sql.SparkSession
 
 
-object read_csv_file
+object print_sparkconfig
 {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder()
-      .appName("projec-1")
-      //.config("spark.master","local")
+      .appName("project-1")
+      .config("spark.master","local")
       .getOrCreate()
 
     //val dataDF = spark.read.json("src/main/resources/data/movies.json")
